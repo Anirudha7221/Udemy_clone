7095b70370109c67fb04a1e3ac8c4ffe51aad713
unknown <Anirudha7221@example.com>
2024-08-21 19:49:34 +0530

    21 Aug

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Navbar=()=>{
+
+    return(
+        <div id="navbar">
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJXzeImAiH1KttGE_h6Si1V2WwaiGPpBUGXQ&s"/>
+       <p>Categories</p>
+       <div id="searchbar">
+            <span class="material-symbols-outlined">
+                search
+            </span>
+           <input type="text" placeholder="Search for anything"/>
+       </div>
+       <div id="content">
+            <p>Plans & Pricing</p>
+            <p>Udemy Business</p>
+            <p>Teach on Udemy</p>
+       </div>
+        <div id="nav-buttons">
+            <span class="material-symbols-outlined">
+                shopping_cart
+             </span>
+            <button>Login</button>
+            <button id="signup">SignUp</button>
+            <button>
+                <span class="material-symbols-outlined">
+                 language
+                </span>
+            </button>
+        </div>
+    </div>
+    )
+}
+
+export default Navbar;
