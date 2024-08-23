27f5201b14342f29f64e6f1c52926b32aee3674e
unknown <Anirudha7221@example.com>
2024-08-23 18:00:16 +0530

    23 Aug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
-import Footer from './components/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import Hero_containeer from './components/Hero-cotaineer/Hero_cotaineer';
 import Section_1 from './components/Section_1/Section_1';
 import Section_2 from './components/Section_2/Section_2';
+import Section_3 from './components/Section_3/Section_3';
+import Section_6 from './components/Section_6/Section_6';
+import Section_4 from './components/Section_4/Section_4';
 import './App.css';
 
 function App() {
+  
   return (
      <div className="App">
         <Navbar></Navbar>
         <Hero_containeer></Hero_containeer>
         <Section_1></Section_1>
         <Section_2></Section_2>
+        <Section_3></Section_3>
+        <Section_4></Section_4>
+        <Section_6></Section_6>
         <Footer></Footer>
     </div> 
   );
---
+++ src/components/Section_6/Section_6.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import './section_6.css';
+
+function Section_6(){
+
+    return(
+        <div id="section-6">
+        <h1>Trending Now</h1>
+        <div>
+            <div id="chatGPT">
+                <h1>ChatGPT is a top skill</h1>
+                <div class="active">
+                    <h2>See ChatGPT Courses</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>3,396,681 learners</p>
+                <div id="trending-skills">
+                    <p>Show all trending skills</p>
+                    <span class="material-symbols-outlined">
+                        trending_up
+                    </span>
+                </div>
+            </div>
+            <div id="development">
+                <h1>Development</h1>
+                <div class="active">
+                    <h2>Python</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>45,354,923 learners</p>
+                <div class="active">
+                    <h2>Web development</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>13,461,690 learners</p>
+                <div class="active">
+                    <h2>Data Science</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>7,345,258 learners</p>
+            </div>
+            <div id="design">
+                <h1>Design</h1>
+                <div class="active">
+                    <h2>Blender</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>2,713,245 learners</p>
+                <div class="active">
+                    <h2>Graphics design</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>4,277,921 learners</p>
+                <div class="active">
+                    <h2>User Expericese (UX) Design</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>1,970,717 learners</p>
+            </div>
+            <div id="business">
+                <h1>Business</h1>
+                <div class="active">
+                    <h2>PMI Project Manegement professional (PMP)</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>2,351,270 learners</p>
+                <div class="active">
+                    <h2>Microsoft Power BI</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>4,209,673 learners</p>
+                <div class="active">
+                    <h2>Project Mangement</h2>
+                    <span class="material-symbols-outlined">
+                        chevron_right
+                    </span>
+                </div>
+                <p>3,622,567 learners</p>
+            </div>
+        </div>
+   </div>
+    )
+}
+
+export default Section_6;
